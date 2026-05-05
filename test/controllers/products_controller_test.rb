3dc36dc764b04ade66a46e082551576091a66f87
Avni Agarwal <avni.agarwal@bornwest.com>
2026-05-05 19:29:14 +0000

    Validation!

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -17,7 +17,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference("Product.count") do
-      post products_url, params: { product: {  title: @product. title, description: @product.description, price: @product.price } }
+      post products_url, params: { product: {  title: @title, description: @product.description, image: file_fixture_upload("lorem.jpg", "image/jpeg"), price: @product.price } }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -34,7 +34,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: {  title: @product. title, description: @product.description, price: @product.price } }
+    patch product_url(@product), params: { product: {  title: @title, description: @product.description, image: file_fixture_upload("lorem.jpg", "image/jpeg"), price: @product.price } }
     assert_redirected_to product_url(@product)
   end
 
