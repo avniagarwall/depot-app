6d5c0f7acd664bcb8651753829aaa6cc9fa39e42
Avni Agarwal <avni.agarwal@bornwest.com>
2026-05-08 02:03:29 +0000

    Added Callbacks to Active Records

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,12 +13,12 @@ class Product < ApplicationRecord
       errors.add(:image, "must be a GIF, JPG or PNG image")
     end
   end
-  
+
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates_associated :reviews
 
   has_many :line_items
-  has_many :reviews, dependent: :destroy
+  has_many :reviews, dependent: :destroy, before_add: :check_review_limit, after_add: :log_review_added
   has_and_belongs_to_many :categories
   before_destroy :ensure_not_referenced_by_any_line_item
   
@@ -28,6 +28,17 @@ class Product < ApplicationRecord
       unless line_items.empty?
       errors.add(:base, "Line Items present")
       throw :abort
+    end
+
+    def check_review_limit(review)
+      if reviews.count >= 10
+        errors.add(:base, "Cannot add more than 10 reviews")
+        throw(:abort)
+      end
+    end
+
+    def log_review_added(review)
+      Rails.logger.info "📝 Review added to #{title}"
     end
   end
 
