f4df2e34c3071ac49d2bdac956a58c415aae8f0b
Avni Agarwal <avni.agarwal@bornwest.com>
2026-05-25 13:34:45 +0000

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   private
     def ensure_an_admin_remains
       if User.count.zero?
-      raise Error.new "Can't delete last user"
+        raise Error.new "Can't delete last user"
     end
   end
 end
