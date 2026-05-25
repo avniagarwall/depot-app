1d01c6956f2617b265cdfdae2656ea2e526b6171
Avni Agarwal <avni.agarwal@bornwest.com>
2026-05-25 14:20:17 +0000

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
+  ADMIN_EMAIL = "admin@depot.com".freeze
   VALIDATE_EMAIL_REGEX = /\A[^@\s]+@[^@\s]+\.[^@\s]+\z/
+
   validates :name, presence: true
-
-  # Email uniqueness (already present) + case insensitive
   validates :email_address, presence: true, uniqueness: { case_sensitive: false }
-
-  # Email format
   validates :email_address, format: {
     with: VALIDATE_EMAIL_REGEX,
     message: :invalid_email
@@ -16,10 +14,10 @@ class User < ApplicationRecord
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
-  after_create  :send_welcome_email   # Exercise 3
-  before_update :prevent_admin_update # Exercise 5
-  before_destroy :prevent_admin_destroy   # Exercise 4
-  after_destroy :ensure_an_admin_remains  # existing
+  after_create  :send_welcome_email
+  before_update :prevent_admin_update
+  before_destroy :prevent_admin_destroy
+  after_destroy :ensure_an_admin_remains
 
   class AdminDeletionError < StandardError; end
 
@@ -30,22 +28,22 @@ class User < ApplicationRecord
     end
 
     def prevent_admin_update
-      if email_address_was == "admin@depot.com"
-        errors.add(:base, "Cannot modify the admin user")
+      if email_address_was == ADMIN_EMAIL
+        errors.add(:base, :cannot_modify_admin)
         throw :abort
       end
     end
 
     def prevent_admin_destroy
-      if email_address == "admin@depot.com"
-        errors.add(:base, "Cannot delete the admin user")
+      if email_address == ADMIN_EMAIL
+        errors.add(:base, :cannot_delete_admin)
         throw :abort
       end
     end
 
     def ensure_an_admin_remains
       if User.count.zero?
-        raise AdminDeletionError, Il8n.t("errors.admin_deletion")
+        raise AdminDeletionError, I18n.t("errors.messages.admin_deletion")
       end
     end
 end
